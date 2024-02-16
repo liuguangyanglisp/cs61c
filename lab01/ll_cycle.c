b0c40c1a8acd357db3b800306dc72a6a748d4207
liuguangyang <liuguangyang1994@gmail.com>
2024-02-16 18:33:22 +0800

    finish lab01

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -3,5 +3,18 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
-    return 0;
+	node *tortoise;
+	node *hare;
+	tortoise = head;
+	hare = head;
+
+
+	while (hare != NULL && hare->next != NULL)  {
+	hare = hare->next->next;
+	tortoise = tortoise->next;
+	if (hare == tortoise) {
+        return 1;
+		}
+	}
+	return 0;
 }
