f6077e443347ae16b3a86719e3b01dd7058c4e31
liuguangyang <liuguangyang1994@gmail.com>
2024-02-17 16:45:04 +0800

    finish lab02 exercise1&2.

--- lab02/bit_ops.c
+++ lab02/bit_ops.c
@@ -9,7 +9,11 @@ unsigned get_bit(unsigned x,
     // Returning -1 is a placeholder (it makes
     // no sense, because get_bit only returns 
     // 0 or 1)
-    return -1;
+    unsigned rightshift_n = x >> n;
+    unsigned leftshift_31 = rightshift_n << 31;
+    unsigned rightshift_31 = leftshift_31 >> 31;
+    unsigned result = 0 | rightshift_31;
+    return result;
 }
 // Set the nth bit of the value of x to v.
 // Assume 0 <= n <= 31, and v is 0 or 1
@@ -17,11 +21,21 @@ void set_bit(unsigned * x,
              unsigned n,
              unsigned v) {
     // YOUR CODE HERE
+    unsigned notx = ~ *x;
+    unsigned notx_nthbit1 = notx | (1 << n);
+    unsigned x_nthbit_0 = ~ notx_nthbit1;
+    unsigned nthbit_value = v << n;
+    unsigned result = x_nthbit_0 | nthbit_value;
+    *x = result;
 }
+
 // Flip the nth bit of the value of x.
 // Assume 0 <= n <= 31
 void flip_bit(unsigned * x,
               unsigned n) {
     // YOUR CODE HERE
+    unsigned nthBitValue = get_bit(*x, n);
+    unsigned flipValue = ((~nthBitValue) << 31) >> 31;
+    set_bit(x, n, flipValue);
 }
 
--- lab02/lfsr.c
+++ lab02/lfsr.c
@@ -4,7 +4,34 @@
 #include <string.h>
 #include "lfsr.h"
 
-void lfsr_calculate(uint16_t *reg) {
-    /* YOUR CODE HERE */
+uint16_t get_bit(uint16_t x,
+                 uint16_t n) {
+    // YOUR CODE HERE
+    // Returning -1 is a placeholder (it makes
+    // no sense, because get_bit only returns 
+    // 0 or 1)
+    uint16_t rightshift_n = x >> n;
+    uint16_t leftshift_15 = rightshift_n << 15;
+    uint16_t rightshift_15 = leftshift_15 >> 15;
+    uint16_t result = 0 | rightshift_15;
+    return result;
 }
 
+void set_bit(uint16_t * x,
+             uint16_t n,
+             uint16_t v) {
+    // YOUR CODE HERE
+    uint16_t notx = ~ *x;
+    uint16_t notx_nthbit1 = notx | (1 << n);
+    uint16_t x_nthbit_0 = ~ notx_nthbit1;
+    uint16_t nthbit_value = v << n;
+    uint16_t result = x_nthbit_0 | nthbit_value;
+    *x = result;
+}
+
+void lfsr_calculate(uint16_t *reg) {
+    /* YOUR CODE HERE */
+    uint16_t xor = (((get_bit(*reg, 0) ^ get_bit(*reg, 2)) ^ get_bit(*reg, 3)) ^ get_bit(*reg, 5));
+    *reg = *reg >> 1;
+    set_bit(reg, 15, xor);
+}
